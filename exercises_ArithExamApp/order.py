8571a4c76e84425a1926942e541ec09ba78cc918
bltomlin <brendan.tomlinson2@gmail.com>
2022-02-13 23:50:13 -0800

    comparison exercise

---
+++ exercises_ArithExamApp/order.py
@@ -0,0 +1,9 @@
+#Get three large numbers from input and check that they are given in ascending order. The subsequent number must be greater than the previous one.
+
+
+large_one = int(input())
+large_two = int(input())
+large_three = int(input())
+result = large_one < large_two < large_three
+print(result)
+
