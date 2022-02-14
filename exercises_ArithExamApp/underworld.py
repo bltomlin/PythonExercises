e98f0c121f228d67ee74d2fde211eec0edb2f655
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-02-14 13:23:44 -0800

    Create underworld.py

---
+++ exercises_ArithExamApp/underworld.py
@@ -0,0 +1,12 @@
+#Charon the Ferryman carries souls across the river Styx to the world of the dead but does so only for a fee. It's a business after all.
+
+#Check whether the recently deceased has a coin, if any, print Welcome to Charon's boat!
+
+#The variable coin stores a Boolean value, so it qualifies as a condition. If coin has False value, alas! There's nothing to be done about it.
+
+#In conclusion, let's warn everyone in the underworld (both those in the boat and those overboard) by printing the message There is no turning back.
+
+coin = bool(int(input()))
+if coin:
+    print("Welcome to Charon's boat!")
+print('There is no turning back.')
