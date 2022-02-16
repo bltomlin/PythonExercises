edba1eca6aa1552a0fbd177778dd1420216fb281
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-02-15 21:07:13 -0800

    Create listfrominput.py

---
+++ exercises_ArithExamApp/listfrominput.py
@@ -0,0 +1,10 @@
+#Write a program that takes n numbers as input, creates a list from them, and then prints it.
+
+
+
+n = int(input())
+result_z = []
+for i in range(n):
+    z = int(input())
+    result_z.append(z)
+print(result_z)
