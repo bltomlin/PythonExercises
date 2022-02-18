85ca504ec5afd422a0bb46928b3c6329cbfe9c7f
bltomlin <brendan.tomlinson2@gmail.com>
2022-02-17 23:30:01 -0800

    another commit for today

---
+++ exercises_ArithExamApp/printingthepath.py
@@ -0,0 +1,4 @@
+#Suppose you need to store a path to a file in lowercase, using the built-in lower() function for this purpose. Look at the code below: when printing path, will we get the string c:\users\public\desktop\temporary\newsletters? If not, just fix the string we pass to path; you do not need to print it.
+
+path = "C:\\Users\\Public\\Desktop\\Temporary\\Newsletters".lower()
+
