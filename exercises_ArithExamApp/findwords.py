251e8b550d0505d596983a7f608b10e4021bb0a1
bltomlin <brendan.tomlinson2@gmail.com>
2022-02-19 20:34:17 -0800

    simplified solution

--- exercises_ArithExamApp/findwords.py
+++ exercises_ArithExamApp/findwords.py
@@ -1,13 +1,4 @@
 #Find all words that end in "s" and print them together separated by an underscore. 
 
-sentence = input()
-array = []
-answer = []
-array = sentence.split()
-for index in array:
-    for character in index:
-        if index[-1] == 's':
-            answer.append(index)
-            break
-answer = '_'.join(answer)
-print(answer)
+print("_".join([element for element in input().split() if element.endswith("s")]))
+
