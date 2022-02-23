7e06153e8cb290e8d7b4d7b7b18669ced6fd6df0
bltomlin <brendan.tomlinson2@gmail.com>
2022-02-22 23:49:35 -0800

    solved

---
+++ exercises_ArithExamApp/minandmax.py
@@ -0,0 +1,10 @@
+#Write the code that receives two integers as its input, each number goes on a new line.
+
+x = int(input())
+y = int(input())
+if x <= y:
+    print(y)
+    print(x)
+else:
+    print(x)
+    print(y)
