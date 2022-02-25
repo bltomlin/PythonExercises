07df805020eb5e78e976422d7cff15ecf4b581dc
bltomlin <brendan.tomlinson2@gmail.com>
2022-02-24 23:18:35 -0800

    solved

---
+++ exercises_ArithExamApp/capitalizeallwords.py
@@ -0,0 +1,7 @@
+#Write a program that takes a string, capitalizes all words in it and then prints the result. Use capwords function from the string module.
+
+from string import capwords
+
+input_string = input()
+print(capwords(input_string))
+
