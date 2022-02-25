0bb264e16766bdbb029383395820f445de4ffc2c
bltomlin <brendan.tomlinson2@gmail.com>
2022-02-24 19:33:10 -0800

    solved

---
+++ exercises_ArithExamApp/workingwithstrings.py
@@ -0,0 +1,6 @@
+#In the string module, there are constants digits and ascii_lowercase. Print digits in the first line of the output, and ascii_lowercase in the second line.
+
+from string import digits, ascii_lowercase
+
+print(digits)
+print(ascii_lowercase)
