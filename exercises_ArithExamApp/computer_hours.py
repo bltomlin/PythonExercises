a5860af0f981c822a9c10721f1494d61d434f5db
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-02-28 17:48:03 -0800

    solved

--- exercises_ArithExamApp/computer_hours.py
+++ exercises_ArithExamApp/computer_hours.py
@@ -3,7 +3,7 @@
 hours = int(input())
 if hours < 2:
     print("That's rare nowadays!")
-elif 2 >= hours < 4:
+elif hours >= 2 and hours < 4:
     print("This seems reasonable")
 else:
     print("Don't forget to take breaks!")
