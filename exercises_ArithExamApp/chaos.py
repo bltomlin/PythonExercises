e113924b791100afcfd811e6c9dc685de88fbb1b
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-03 18:18:07 -0800

    Create chaos.py

---
+++ exercises_ArithExamApp/chaos.py
@@ -0,0 +1,6 @@
+#Look at the code below and eliminate the chaos: the first line should print the resulting number of the calculation and the second line should print the word mathematics.
+
+
+
+print(45/9 + 16*(5 + 8))
+print('mathematics')
