4ae0132f146eca4e239236e5c2052ad801e497fc
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-03 21:59:22 -0800

    Create fix_the_exceptions.py

---
+++ exercises_ArithExamApp/fix_the_exceptions.py
@@ -0,0 +1,8 @@
+#Below is a program that outputs different results depending on the specified age. What is wrong in the current program? Find and correct the mistake.
+
+age = 20
+
+if age < 18:
+    print("You can't watch Game of Thrones!")
+else:
+    print("You are welcome to be here. Sit, have a drink and enjoy the show!")
