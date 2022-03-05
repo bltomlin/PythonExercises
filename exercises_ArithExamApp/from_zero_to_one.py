7808c757bf8cf16ae2dc72eea8da9a1d2e4c627d
bltomlin <brendan.tomlinson2@gmail.com>
2022-03-04 23:56:57 -0800

    solved

---
+++ exercises_ArithExamApp/from_zero_to_one.py
@@ -0,0 +1,8 @@
+#The function used to set a starting point of the random search algorithm is called random.seed() and takes a single integer number. Write a program that takes an integer number n, sets n as a seed and then prints a pseudo-random number from 0 to 1.
+#The variable n is already defined.
+
+import random
+
+n = int(input())
+random.seed(n)
+print(random.random())
