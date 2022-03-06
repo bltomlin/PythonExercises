f4468fc31fd7eb4727d0bef2e8aeed67ca2e8643
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-05 22:10:30 -0800

    Create poetry.py

---
+++ exercises_ArithExamApp/poetry.py
@@ -0,0 +1,3 @@
+#Write a program that will create and print this string from "How The Grinch Stole Christmas":
+
+print("Did that stop the old Grinch?\nNo! The Grinch simply said,\n\"If I can't find a reindeer,\nI'll make one instead!\"")
