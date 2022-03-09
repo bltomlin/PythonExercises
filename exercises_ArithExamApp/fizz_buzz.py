1c2a8e34374697bdb87b1a651b213ff9cc600e32
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-08 23:28:35 -0800

    Update fizz_buzz.py

--- exercises_ArithExamApp/fizz_buzz.py
+++ exercises_ArithExamApp/fizz_buzz.py
@@ -1,10 +1,10 @@
 #FizzBuzz is a famous code challenge used in interviews to test basic programming skills. It's time to write your own implementation.
-number = int(input())
-
-if number % 3 == 0 and number % 5 != 0:
-	print("Fizz")
-if number % 5 == 0 and number % 3 != 0:
-	print("Buzz")
-if number % 5 == 0 and number % 3 == 0:
-	print("FizzBuzz")
-print(range(number + 1, 100))
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 != 0:
+        print("Fizz")
+    elif number % 5 == 0 and number % 3 != 0:
+        print("Buzz")
+    elif number % 5 == 0 and number % 3 == 0:
+        print("FizzBuzz")
+    else:
+        print(number)
