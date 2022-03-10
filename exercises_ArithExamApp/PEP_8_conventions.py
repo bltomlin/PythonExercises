2a17df6d78815a96c8b24be06a3152635f5bf253
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-09 22:34:35 -0800

    Update PEP_8_conventions.py

--- exercises_ArithExamApp/PEP_8_conventions.py
+++ exercises_ArithExamApp/PEP_8_conventions.py
@@ -3,9 +3,11 @@
 #Check if the variable name equals 'l' (lowercase letter el), 'O' (uppercase letter oh), or 'I' (uppercase letter eye). If it does, print "Never use the characters 'l', 'O', or 'I' as single-character variable names". If it does not, move to the next step.
 #Check if the name is all lowercase or uppercase. If all of its characters are lowercase, print the message "It is a common variable". If all of its characters are uppercase, print the message "It is a constant". In other cases, print the message "You shouldn't use mixedCase".
 
+name = input()
+
 def check_name(name):
-    forbidden = ['l', '0', 'I']
-    if forbidden in name):
+    forbidden = set('lOI')
+    if any((c in forbidden) for c in name):
         print("Never use the characters 'l', 'O', or 'I' as single-character variable names")
     elif name.islower():
         print("It is a common variable")
