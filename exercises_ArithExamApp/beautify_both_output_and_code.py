0806569a24aec6a38c46e6638799b91c99ad0dad
bltomlin <brendan.tomlinson2@gmail.com>
2022-03-12 23:38:42 -0800

    solved

---
+++ exercises_ArithExamApp/beautify_both_output_and_code.py
@@ -0,0 +1,6 @@
+#Read the nickname and profession of the user from the input and print a user-specific URL. Don't bother about any rules of composing the URLs and just use raw input to accomplish the task.
+
+name = input()
+profession = input()
+print(f'http://example.com/{name}/desirable/{profession}/profile')
+
