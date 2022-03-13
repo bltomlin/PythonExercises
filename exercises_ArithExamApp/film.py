401eb0aedf8774b6b2d873a6bd8d8e6b800822ee
bltomlin <brendan.tomlinson2@gmail.com>
2022-03-12 23:34:12 -0800

    solved

---
+++ exercises_ArithExamApp/film.py
@@ -0,0 +1,8 @@
+#Write a program that gets information about a movie from the input and presents in the following format:
+
+#movie (dir. director) came out in year
+
+movie = input()
+director = input()
+year = input()
+print(f'{movie} (dir. {director}) came out in {year}')
