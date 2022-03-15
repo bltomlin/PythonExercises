dc48fda0312abf1f036115f3e8e4958aa45f86ae
bltomlin <brendan.tomlinson2@gmail.com>
2022-03-14 22:41:36 -0700

    solved

---
+++ exercises_ArithExamApp/exactly_100_times.py
@@ -0,0 +1,5 @@
+#Jane knows that the variable n stores some integer number (for example, 12345) and wants to print it exactly 100 times in one line. Help her and write down a single line of code that will print number n exactly 100 times.
+
+n = 12345
+print(str(n) * 100)
+
