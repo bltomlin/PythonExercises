361888a124e109a3e55aa77949eb73a1846dd0b4
bltomlin <brendan.tomlinson2@gmail.com>
2022-03-14 22:44:30 -0700

    solved

---
+++ exercises_ArithExamApp/lexical_reduplication.py
@@ -0,0 +1,4 @@
+#We have a full reduplication here since the entire word is repeated. That's the mechanism we want you to implement. Print a word exactly 2 times. The spelling rules vary across the globe, so do not separate the halves
+
+word = input()
+print(word * 2)
