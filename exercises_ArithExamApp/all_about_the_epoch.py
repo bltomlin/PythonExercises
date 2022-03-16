9677f39d6d6c00d496340302113f024ad5d8b3bf
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-15 17:47:51 -0700

    Create all_about_the_epoch.py

---
+++ exercises_ArithExamApp/all_about_the_epoch.py
@@ -0,0 +1,5 @@
+#You and your friend study Python together. You told them about the time module and the epoch concept in it. Now they wonder when the epoch on their system starts. Could you kindly show them how to check that? Please, print the result.
+
+import time
+
+print(time.gmtime(0))
