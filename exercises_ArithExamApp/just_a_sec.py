5675ef2674cca509476cfc58ae462d69d83bc7d8
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-15 17:39:04 -0700

    Update just_a_sec.py

---
+++ exercises_ArithExamApp/just_a_sec.py
@@ -0,0 +1,11 @@
+#Correct the given program so it would print the second sentence 10 seconds after the first one.
+
+import time
+
+sent_1 = "I'm the first sentence."
+print(sent_1)
+
+time.sleep(10)
+
+sent_2 = "And I'm the second sentence."
+print(sent_2)
