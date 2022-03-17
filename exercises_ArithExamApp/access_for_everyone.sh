ea478aaa73eecc71af183da7b7ee4cc830545311
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-16 19:02:47 -0700

    Added questions

--- exercises_ArithExamApp/access_for_everyone.sh
+++ exercises_ArithExamApp/access_for_everyone.sh
@@ -1,3 +1,7 @@
+#Use the chmod command and absolute mode to set permissions to read, write and execute the /tmp/test.exe file for all the users.
+
+
+
 solve() {
     chmod 777 /tmp/test.exe
 
--- exercises_ArithExamApp/change_other_permission.sh
+++ exercises_ArithExamApp/change_other_permission.sh
@@ -1,3 +1,5 @@
+#Imagine you have a project file called /tmp/program.py. Your task is to make this file executable for other users without overriding previous permissions. Use the chmod command and symbolic mode to do the task.
+
 solve() {
     chmod o+x /tmp/program.py
 
