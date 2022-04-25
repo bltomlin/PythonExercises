67f23ac69a96d8e63bf1d49ad928c5dbd7771c97
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-04-24 23:09:33 -0700

    Solved this problem. It was a little difficult due to the argparse commands.. but I figured it out.

---
+++ exercises_loancalc/caesarcipher.py
@@ -0,0 +1,26 @@
+"""
+write a program that takes a file as an argument, reads it, decodes it, and prints the decoded text.
+"""
+
+import argparse
+
+
+def decode_caesar_cipher(s, n):
+    alpha = " ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',.?!"
+    s = s.strip()
+    text = ''
+    for c in s:
+        text += alpha[(alpha.index(c) + n) % len(alpha)]
+    print('Decoded text: "' + text + '"')
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file")
+args = parser.parse_args()
+filename = args.file
+opened_file = open(filename)
+encoded_text = opened_file.read()
+opened_file.close()
+
+decode_caesar_cipher(encoded_text, -13)
+
