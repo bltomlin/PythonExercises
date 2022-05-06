68305236b58aa4fa7aa44dc73a7da48fda1aa6d7
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-05-05 22:05:22 -0700

    Create update_info.py
    solved

---
+++ exercises_webscrapper/update_info.py
@@ -0,0 +1,9 @@
+"""
+Let's say you have a dict with children's names and professions they would like to have when they grow up:
+
+children = {'Emily': 'artist', 'Adam': 'astronaut', 'Nancy': 'programmer'}
+But what if someone's choice has changed? Say, Emily now wants to be a musician. Update the dict and print its new version.
+"""
+
+children['Emily'] = 'musician'
+print(children)
