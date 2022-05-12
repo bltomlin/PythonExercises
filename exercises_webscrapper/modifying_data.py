4a3c98e6e9bc9324f1b4ce84ddb4935782a3a0eb
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-05-11 23:06:05 -0700

    Create modifying_data.py
    solved

---
+++ exercises_webscrapper/modifying_data.py
@@ -0,0 +1,9 @@
+"""
+Lists, unlike strings, are mutable. We can use that to modify their data with indexes.
+
+There is a list planets with the names of the Solar system planets. However, instead of the 5th planet, there's an X. Replace it with the real name of the 5th planet. Do not forget, the first letter must be a capital one!
+
+Note that the list planets has already been defined, you just need to change one element. Mind its index!
+"""
+
+planets[4] = 'Jupiter'
