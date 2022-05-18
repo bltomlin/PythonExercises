3d0f00bcb20c39a98f0b944f30e556240e9d040f
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-05-17 17:36:04 -0700

    Update dictionary_methods.py
    solved

--- exercises_webscrapper/dictionary_methods.py
+++ exercises_webscrapper/dictionary_methods.py
@@ -1,10 +1,18 @@
-key = int(input())
+"""
+Anthony keeps track of what he eats: he writes down how many calories are in his meals. Use the list of dictionaries to calculate the total amount of calories per day and print it.
+The sample input will look like:
+"""
+meals = [
+        {"title": "Oatmeal pancakes with apple and cinnamon", "kcal": 370},
+        {"title": "Italian salad with fusilli and ham", "kcal": 320},
+        {"title": "Bulgur with vegetables", "kcal": 350},
+        {"title": "Curd souffle with lingonberries and ginger", "kcal": 225},
+        {"title": "Oatmeal with honey and peanuts", "kcal": 440}]
+"""
+The output in this case should be 1705.
+"""
 
-try:
-    del squares[key]      
-except KeyError:
-    print("There is no such key")
-else:
-    print(key * key)
-
-print(squares)
+total_kcal = 0
+for i in meals:
+    total_kcal += i.get("kcal")
+print(total_kcal)
