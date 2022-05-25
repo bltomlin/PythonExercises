33fdb9c7b509587e06049e17415e2e7e1448a0d9
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-05-24 23:29:50 -0700

    Create operations_with_dictionary.py
    solvedf

---
+++ exercises_webscrapper/operations_with_dictionary.py
@@ -0,0 +1,9 @@
+"""
+Replace the dots with membership operators so that the output result would always be True.
+"""
+
+sidekicks = {'Don Quixote': 'Sancho Panza', 'Batman': 'Robin', 'Holmes': 'Watson'}
+print('Sancho Panza' not in sidekicks)
+print('Batman' in sidekicks)
+print('Han Solo' not in sidekicks)
+print('' not in sidekicks)
