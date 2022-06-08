0bec0965df82595ba511dbfc5dfa52c9cee4dba7
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-06-07 22:18:31 -0700

    Update get_the_subtitles.py

--- exercises_webscrapper/get_the_subtitles.py
+++ exercises_webscrapper/get_the_subtitles.py
@@ -1,3 +1,14 @@
+"""
+Your task is to find and print a subtitle of the article by an index. You are given two input values:
+
+The first one is the index of the subtitle. It starts from 0 in Python. You do not need to subtract one from the given number.
+The second one is the the link to the article.
+The subtitles are stored in the <h2> tag. For example, your code should print The Definite Article for the following input:
+
+1
+https://www.grammarly.com/blog/articles/
+"""
+
 import requests
 
 from bs4 import BeautifulSoup
