3f7fd36666e031b39db2b14980d2db0c1f63c95c
bltomlin <62484274+bltomlin@users.noreply.github.com>
2022-06-16 22:04:06 -0700

    Create to_bytes.py
    solved

--- exercises_webscrapper/to_bytes.py
+++ exercises_webscrapper/to_bytes.py
@@ -1,4 +1,11 @@
-usr_input = int(input())
-bytes_num = bytes(usr_input)
-ans = int.from_bytes(bytes_num, 'little')
+"""
+Write a code that takes a given number, converts it to a bytes object and outputs the sum of its bytes.
+
+A little spoiler: the test numbers are going to be greater than 255, so you're going to need at least 2 bytes to encode them. 
+"""
+
+usr_input = (int(input())).to_bytes(2, byteorder='little')
+ans = 0
+for i in usr_input:
+    ans += i
 print(ans)
