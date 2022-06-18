bba8fa5d5743f69c96ed1a768e13d882c7faa6ba
bltomlin <62484274+bltomlin@users.noreply.github.com>
2022-06-18 00:25:06 -0700

    Create capitalize_2.py
    solved

---
+++ exercises_webscrapper/capitalize_2.py
@@ -0,0 +1,8 @@
+"""
+Read input twice. Capitalize only the first word In the first string. After this, capitalize each word in the second one. Print the resulting strings each on a new line.
+"""
+
+import string
+
+print(str.capitalize(input()))
+print(string.capwords(input()))
