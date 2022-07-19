55fec1bf45415b4cf1b2ca828cc3bc3d0e8ce212
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-07-18 22:57:38 -0700

    creates a list of lengths

---
+++ exercises_markdowneditor/length.py
@@ -0,0 +1,6 @@
+"""
+Given a list of words in the code below, create a list of lengths of those words and print it.
+"""
+words = ["apple", "it", "creek", "pelican", "subsequent", "horse",
+         "apothecary"]
+print([len(x) for x in words])
